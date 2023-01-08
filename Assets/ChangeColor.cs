555cb0ae1bb38f9738d41c0df890ee5e5fc6bf86
Icy-Scream <jdpetta21@gmail.com>
2023-01-08 09:38:08 -1000

    Add Project

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ChangeColor : MonoBehaviour
+{
+    private GameActions _input;
+    Ray ray;
+    RaycastHit hit;
+    private void Awake()
+    {
+        _input = new GameActions();
+    }
+
+
+    private void OnEnable()
+    {
+        _input.Player.Enable();
+        _input.Player.ChangeColor.performed += ChangeColor_performed;
+    }
+
+    private void ChangeColor_performed(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Ray");
+        ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray,out hit)) 
+        {
+            hit.transform.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+}
